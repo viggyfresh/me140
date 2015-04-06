9afb19ddffe518bf0a3831f73dab574372f7745b
Richie Tran <rtran15@stanford.edu>
2015-04-06 15:31:05 -0700

    added turbine var file

---
+++ turbine_var.m
@@ -0,0 +1,17 @@
+function [ vars ] = turbine_var( vars )
+%This is currently the same as turbine.m, will use as template to for
+%variable prameter
+% 
+
+W_comp = vars.m_dot_core.*vars.c_p.*(vars.T_03-vars.T_013);
+W_fan = vars.m_dot.*vars.c_p.*(vars.T_013-vars.T_02);
+W_comp_and_fan = W_comp + W_fan;
+
+vars.T_05 = vars.T_04-W_comp_and_fan./(vars.m_dot_core.*vars.c_p);
+vars.T_05s = (vars.c_p.*vars.T_04-(vars.c_p.*vars.T_04-vars.c_p.*vars.T_05)./vars.eta_turb)./vars.c_p;
+
+
+vars.P_05 = vars.P_04.*(vars.T_05s./vars.T_04).^(vars.k./(vars.k-1));
+
+end
+
