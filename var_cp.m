986c6c13071079687a6628c0eee8324d38293e53
Zach Stuart <zstuart@stanford.edu>
2015-04-05 21:34:01 -0700

    destroy everything

---
+++ var_cp.m
@@ -0,0 +1,16 @@
+function [T] = var_cp(T1,P2_over_P1)
+R = 286.9;
+cp_2_calc = 0;
+cp_2_guess = 500;
+count = 0;
+while abs(cp_2_calc-cp_2_guess)>0.01
+    cp_2_guess = cp_2_calc;
+    cp_avg = 1/2 .* ( sp_heats(T1) + cp_2_guess )
+    T2_calc = T1 .* (P2_over_P1).^(R./cp_avg);
+    cp_2_calc = sp_heats(T2_calc);
+    count = count+1;
+end
+cp = cp_2_calc;
+T = T2_calc;
+end
+
