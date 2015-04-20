19e7cfc79d3fcccfc67228448571d154646ab2fb
Zach Stuart <zstuart@stanford.edu>
2015-04-19 23:46:59 -0700

    Robustification

--- proj3/deltaH_var_cp.m
+++ proj3/deltaH_var_cp.m
@@ -4,40 +4,10 @@ dT=.01;
 T=T1;
 deltaH=0;
 
-%O2
-if (strcmp(type, 'O2'))
-    while T < T2
-        T = T+dT;
-        increment = sp_heats_O2(T) .* dT;
-        deltaH = deltaH + increment;
-    end
-end
-
-%N2
-if (strcmp(type, 'N2'))
-    while T < T2
-        T = T+dT;
-        increment = sp_heats_N2(T) .* dT;
-        deltaH = deltaH + increment;
-    end
-end
-
-%CO2
-if (strcmp(type, 'CO2'))
-    while T < T2
-        T = T+dT;
-        increment = sp_heats_CO2(T) .* dT;
-        deltaH = deltaH + increment;
-    end
-end
-
-%H2
-if (strcmp(type, 'H2O'))
-    while T < T2
-        T = T+dT;
-        increment = sp_heats_H2O(T) .* dT;
-        deltaH = deltaH + increment;
-    end
+while T < T2
+    T = T+dT;
+    increment = sp_heats(T,type) .* dT;
+    deltaH = deltaH + increment;
 end
 
 end
