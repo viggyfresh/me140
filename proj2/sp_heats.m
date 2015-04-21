8da7c5aa6e2dca8a21e493b66fa41f47dc636923
Zach Stuart <zstuart@stanford.edu>
2015-04-20 17:19:14 -0700

    changed proj2 to constant cp woot

--- proj2/sp_heats.m
+++ proj2/sp_heats.m
@@ -1,12 +1,17 @@
 function [cp, cv, gamma, R] = sp_heats(temp)
 
 R = 286.9;
-molarMass = 28.97;
+molarMass = 1;
+%molarMass = 28.97;
 
-a = 28.11;
-b = 0.1967*10^-2;
-c = 0.4802*10^-5;
-d = -1.966*10^-9;
+a = 1.0038;
+b = 0;
+c = 0;
+d = 0;
+% a = 28.11;
+% b = 0.1967*10^-2;
+% c = 0.4802*10^-5;
+% d = -1.966*10^-9;
 
 p = [d c b a];
 cp = (polyval(p,temp)) * (1000 / molarMass);
