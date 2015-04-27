b884eecacbf74d725aeaa78ef92c068c453cb382
jhahrens <jhahrens@stanford.edu>
2015-04-26 23:52:36 -0700

    question 2

--- proj4/proj4.m
+++ proj4/proj4.m
@@ -10,7 +10,7 @@ MM.H = 1.008;
 MM.H2 = 2*MM.H;
 MM.H2O = 18.016;
 MM.CO2 = MM.C + MM.O2;
-MM.air = 28.97; 
+MM.air = 28.97;
 
 % Enthalpy (J/kg) and entropy (J/(kg*K)) of formation values
 hf.H2O_vap = -241820 / MM.H2O * 1000;
@@ -94,7 +94,7 @@ for i=1:length(T_series)
     mf_react.H2O_vap = m_react.H2O_vap ./ m_react.sum;
     
     % Enthalpy and Gibbs free energy - all in J / kg
-    H.O2 = hf.O2 + integral(fun_O2_h, T_standard, T);  
+    H.O2 = hf.O2 + integral(fun_O2_h, T_standard, T);
     g.O2 = H.O2...
         - T * ((sf.O2 + integral(fun_O2_s, T_standard, T))...
         - R * log(P/P_standard));
@@ -163,7 +163,7 @@ end
 % Plotting for Part 1
 figure;
 plot(T_series, efficiency.LHV * 100, T_series, efficiency.HHV * 100,...
-     T_series, efficiency.actual * 100, T_series, n_carnot);
+    T_series, efficiency.actual * 100, T_series, n_carnot);
 xlabel('Temperature (K)');
 ylabel('Efficiency (%)');
 legend('LHV', 'HHV', '\DeltaH', '\eta_c');
@@ -171,11 +171,178 @@ title('First Law Efficiency vs. Temperature');
 set(gcf, 'color', 'w');
 plotfixer;
 
-%% Part 2 
+%% Part 2
 
-T_values = [80,220,650,800] + 273; % celsius
-lambda_range = 1:.1:10;
-P_range = 1:.1:40; %Pa
+T_values = [80,220,650,800] + 273; % K
+lambda_range = 1:1:10;
+P_range = 1:1:40; % Pa
+
+Rvar.O2 = R/MM.O2 * 10^3;
+Rvar.H2 = R/MM.H2 * 10^3;
+Rvar.N2 = R/MM.N2 * 10^3;
+Rvar.H2O = R/MM.H2O * 10^3;
 
 % change pressures, hold lamda
-lamda = 2;
+lambda = 2;
+for i=1:length(T_values)
+    T = T_values(i);
+    for j = 1:length(P_range)
+        % Enthalpy and Gibbs free energy - all in J / kg
+        P = P_range(j) * 101.3 * 10^3;
+        H.O2 = hf.O2 + integral(fun_O2_h, T_standard, T);
+        g.O2 = H.O2...
+            - T * ((sf.O2 + integral(fun_O2_s, T_standard, T))...
+            - Rvar.O2 * log(P/P_standard));
+        
+        H.N2 = hf.N2 + integral(fun_N2_h, T_standard, T);
+        g.N2 = H.N2...
+            - T * ((sf.N2 + integral(fun_N2_s, T_standard, T))...
+            - Rvar.N2 * log(P/P_standard));
+        
+        H.H2 = hf.H2 + integral(fun_H2_h, T_standard, T);
+        g.H2 = H.H2...
+            - T * ((sf.H2 + integral(fun_H2_s, T_standard, T))...
+            - Rvar.H2 * log(P/P_standard));
+        
+        H.O2 = hf.O2 + integral(fun_O2_h, T_standard, T);
+        g.O2 = H.O2...
+            - T * ((sf.O2 + integral(fun_O2_s, T_standard, T))...
+            - Rvar.O2 * log(P/P_standard));
+        
+        H.H2O_vap = hf.H2O_vap + integral(fun_H2O_vap_h, T_standard, T);
+        g.H2O_vap = H.H2O_vap...
+            - T * ((sf.H2O_vap + integral(fun_H2O_vap_s, T_standard, T))...
+            - Rvar.H2O * log(P/P_standard));
+        
+        H.H2O_liq = hf.H2O_liq + integral(fun_H2O_liq_h, T_standard, T);
+        g.H2O_liq = H.H2O_liq...
+            - T * ((sf.H2O_liq + integral(fun_H2O_liq_s, T_standard, T))...
+            - Rvar.H2O * log(P/P_standard));
+        
+        % Reactants & Products:
+        g.react = mf_react.N2 .* g.N2 + mf_react.O2 .* g.O2...
+            + mf_react.H2 .* g.H2 + mf_react.H2O_vap .* g.H2O_vap;
+        
+        % Lower heating value - all gas; higher heating value - all liquid
+        g.LHV = mf_prod.N2 .* g.N2 + mf_prod.H2O .* g.H2O_vap...
+            + mf_prod.O2 .* g.O2;
+        g.actual = mf_prod.N2 .* g.N2 +  mf_prod.O2 .* g.O2...
+            + mf_prod.H2O_vap .* g.H2O_vap + mf_prod.H2O_liq .* g.H2O_liq;
+        
+        % Calculate actual enthalpy change for denominator
+        H_prod = mf_prod.N2 * H.N2 + mf_prod.O2 * H.O2...
+            + mf_prod.H2O_vap * H.H2O_vap + mf_prod.H2O_liq * H.H2O_liq;
+        H_react = mf_react.N2 * H.N2 + mf_react.O2 * H.O2...
+            + mf_react.H2 * H.H2 + mf_react.H2O_vap * H.H2O_vap;
+        H_actual = H_prod - H_react;
+        
+        
+        % Calculate efficiencies
+        eta_p(i, j) = -m_react.sum * (g.actual - g.react)...
+            ./ (m_react.H2 * LHV);
+    end
+end
+
+% Plotting for Part 2a
+figure;
+plot(P_range, eta_p(1, :) * 100, P_range, eta_p(2, :) * 100,...
+     P_range, eta_p(3, :) * 100, P_range, eta_p(4, :) * 100);
+xlabel('Pressure (Pa)');
+ylabel('Efficiency (%)');
+legend('80^{\circ}C', '220^{\circ}C', '650^{\circ}C', '800^{\circ}C');
+title('First Law Efficiency vs. Pressure');
+set(gcf, 'color', 'w');
+plotfixer;
+
+% Change lamda, keep pressure constant
+
+%%
+
+for i=1:length(T_values)
+    T = T_values(i);
+    for j = 1:length(lambda_range)
+        % Enthalpy and Gibbs free energy - all in J / kg
+        P = P_standard;
+        lambda = lambda_range(j)
+        m_prod.H2O_vap = beta * MM.H2O;
+        m_prod.H2O_liq = gamma * MM.H2O;
+        m_prod.H2O = MM.H2O;
+        m_prod.O2 = 0.5 * (lambda - 1) * MM.O2;
+        m_prod.N2 = 0.5 * lambda * 3.76 * MM.N2;
+        m_prod.sum = m_prod.H2O + m_prod.O2 + m_prod.N2;
+        
+        mf_prod.H2O_vap = m_prod.H2O_vap ./ m_prod.sum;
+        mf_prod.H2O_liq = m_prod.H2O_liq ./ m_prod.sum;
+        mf_prod.H2O = m_prod.H2O ./ m_prod.sum;
+        mf_prod.N2 = m_prod.N2 ./ m_prod.sum;
+        mf_prod.O2 = m_prod.O2 ./ m_prod.sum;
+        
+        m_react.H2 = 1 * MM.H2;
+        m_react.O2 = 0.5 * lambda * MM.O2;
+        m_react.N2 = 0.5 * lambda * 3.76 * MM.N2;
+        m_react.H2O_vap = alpha * MM.H2O;
+        m_react.sum = m_react.H2 + m_react.O2 + m_react.N2 + m_react.H2O_vap;
+        
+        mf_react.H2 = m_react.H2 ./ m_react.sum;
+        mf_react.O2 = m_react.O2 ./ m_react.sum;
+        mf_react.N2 = m_react.N2 ./ m_react.sum;
+        mf_react.H2O_vap = m_react.H2O_vap ./ m_react.sum;
+        
+        mf_prod
+        
+        H.O2 = hf.O2 + integral(fun_O2_h, T_standard, T);
+        g.O2 = H.O2...
+            - T * ((sf.O2 + integral(fun_O2_s, T_standard, T))...
+            - Rvar.O2 * log(P/P_standard));
+        
+        H.N2 = hf.N2 + integral(fun_N2_h, T_standard, T);
+        g.N2 = H.N2...
+            - T * ((sf.N2 + integral(fun_N2_s, T_standard, T))...
+            - Rvar.N2 * log(P/P_standard));
+        
+        H.H2 = hf.H2 + integral(fun_H2_h, T_standard, T);
+        g.H2 = H.H2...
+            - T * ((sf.H2 + integral(fun_H2_s, T_standard, T))...
+            - Rvar.H2 * log(P/P_standard));
+        
+        H.O2 = hf.O2 + integral(fun_O2_h, T_standard, T);
+        g.O2 = H.O2...
+            - T * ((sf.O2 + integral(fun_O2_s, T_standard, T))...
+            - Rvar.O2 * log(P/P_standard));
+        
+        H.H2O_vap = hf.H2O_vap + integral(fun_H2O_vap_h, T_standard, T);
+        g.H2O_vap = H.H2O_vap...
+            - T * ((sf.H2O_vap + integral(fun_H2O_vap_s, T_standard, T))...
+            - Rvar.H2O * log(P/P_standard));
+        
+        H.H2O_liq = hf.H2O_liq + integral(fun_H2O_liq_h, T_standard, T);
+        g.H2O_liq = H.H2O_liq...
+            - T * ((sf.H2O_liq + integral(fun_H2O_liq_s, T_standard, T))...
+            - Rvar.H2O * log(P/P_standard));
+        
+        % Reactants & Products:
+        g.react = mf_react.N2 .* g.N2 + mf_react.O2 .* g.O2...
+            + mf_react.H2 .* g.H2 + mf_react.H2O_vap .* g.H2O_vap;
+        
+        % Lower heating value - all gas; higher heating value - all liquid
+        g.LHV = mf_prod.N2 .* g.N2 + mf_prod.H2O .* g.H2O_vap...
+            + mf_prod.O2 .* g.O2;
+        g.actual = mf_prod.N2 .* g.N2 +  mf_prod.O2 .* g.O2...
+            + mf_prod.H2O_vap .* g.H2O_vap + mf_prod.H2O_liq .* g.H2O_liq;
+        
+        % Calculate actual enthalpy change for denominator
+        H_prod = mf_prod.N2 * H.N2 + mf_prod.O2 * H.O2...
+            + mf_prod.H2O_vap * H.H2O_vap + mf_prod.H2O_liq * H.H2O_liq;
+        H_react = mf_react.N2 * H.N2 + mf_react.O2 * H.O2...
+            + mf_react.H2 * H.H2 + mf_react.H2O_vap * H.H2O_vap;
+        H_actual = H_prod - H_react;
+        
+        % Calculate efficiencies
+        eta_lambda(i, j) = -m_react.sum * (g.actual - g.react)...
+            ./ (m_react.H2 * LHV);
+    end
+end
+figure
+plot(lambda_range, eta_lambda(1, :) * 100, lambda_range, ...
+    eta_lambda(2, :) * 100, lambda_range, eta_lambda(3, :)...
+    * 100, lambda_range, eta_lambda(4, :) * 100)
